3c9ff76fa2a2cc4d167e032466a4a159792a6ffc
guillermo <guillermo.m.rangel@gmail.com>
2021-06-02 14:14:00 -0400

    Add home route and controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: "home#show"
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
