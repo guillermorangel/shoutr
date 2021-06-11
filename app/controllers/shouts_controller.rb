38509a1422712af38ed98e241805b03db6a66561
guillermo <guillermo.m.rangel@gmail.com>
2021-06-10 23:50:45 -0400

    Persist the user's shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,5 +1,10 @@
 class ShoutsController < ApplicationController
   def create
+    current_user.shouts.create(shout_params)
     redirect_to root_path
+  end
+
+  def shout_params
+    params.require(:shout).permit(:body)
   end
 end
