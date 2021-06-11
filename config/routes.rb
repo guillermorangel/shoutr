3885cb074af9a2784e8a030d1e4a822feb36584d
guillermo <guillermo.m.rangel@gmail.com>
2021-06-10 23:38:52 -0400

    Initial setup to create a shout, add simple form, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
 
   root to: "home#show", as: :root
+  resources :shouts, only: [:create]
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
